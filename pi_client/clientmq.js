3e31a0b58385fe63dde68822b7645cb904981413
elennon <elennon@outlook.ie>
2016-11-02 00:46:07 +0000

    finally

--- pi_client/clientmq.js
+++ pi_client/clientmq.js
@@ -4,7 +4,7 @@ const exec = require('child_process').exec;
 var counter = 1;
 
 var options = {
-    port: '1883',
+    port: '1884',
     clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8)
 };
 var client = mqtt.connect('mqtt://139.59.172.240', options);
@@ -15,7 +15,7 @@ client.on('error', function (error) {
 
 function getBmp180Reading(callback) {
     var reading;
-    exec('python /home/pi/projects/mqtt_client/bmp180.py', (error, stdout, stderr) => {
+    exec('python /home/pi/projects/mqtt_reader/pi_client/scripts/bmp180.py', (error, stdout, stderr) => {
         if (error) {
             return callback(error);
         }
@@ -25,18 +25,19 @@ function getBmp180Reading(callback) {
             createdAt : Date.now(), 
             id : uuid.v4(), 
             ip : "piserial#", 
+            message : "xvvxvx",
             ok : true, 
             sensor : "Bmp180", 
             temp : obj.temp,
             pressure : obj.pressure
         } 
-        callback(null, reading, "Bmp180");
+        callback(null, objason, "Bmp180");
     });
 }
 
 function getHfluxReading(callback) {
     var reading;
-    exec('python /home/pi/projects/mqtt_client/bmp180.py', (error, stdout, stderr) => {
+    exec('python /home/pi/projects/mqtt_reader/pi_client/scripts/bmp180.py', (error, stdout, stderr) => {
         if (error) {
             return callback(error);
         }
@@ -45,12 +46,12 @@ function getHfluxReading(callback) {
         var objason = { 
             createdAt : Date.now(), 
             id : uuid.v4(), 
-            ip : "piserial#", 
+            ip : "piSerial#", 
             ok : true, 
             sensor : "Hflux", 
             val : 34.34
         } 
-        callback(null, reading, "Hflux");
+        callback(null, objason, "Hflux");
     });
 }
 
@@ -59,6 +60,7 @@ function handleResult(err, result, collection) {
         console.error(err.stack || err.message);
         return;
     }
+    //console.log(collection + '.......................' + JSON.stringify(result));
     client.publish(collection, JSON.stringify(result));
     console.log('reading #' + counter);
 }
@@ -69,6 +71,7 @@ function getresp(err, result){
 
 setInterval(function(){
     getBmp180Reading(handleResult);
+    getHfluxReading(handleResult);
     counter++;
 }, 1* 1000);
 
