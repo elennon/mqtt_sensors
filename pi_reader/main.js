c911d3d0db98224661adcd4164f68e78c3399aab
ed <elennon@outlook.ie>
2016-11-12 19:17:50 +0000

    added promise pause 4

--- pi_reader/main.js
+++ pi_reader/main.js
@@ -53,12 +53,6 @@ setInterval(function(){
     sdp610(addSdpVal);
   }, 1* 2000);
 
-suspend(function* () {
-    console.log('Welcome to My Console,');
-    yield setTimeout(suspend.resume(), 10000); // 10 seconds pass..
-    console.log('Blah blah blah blah extra-blah');
-})();
-
 setInterval(function(){
     suspend(function* () {
         bmp180(handleResult);
