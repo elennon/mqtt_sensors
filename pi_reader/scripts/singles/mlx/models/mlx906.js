078de1d5d88100bb5f39396583a1cfd966a7d865
ed <elennon@outlook.ie>
2016-12-03 22:51:27 +0000

    2 more

---
+++ pi_reader/scripts/singles/mlx/models/mlx906.js
@@ -0,0 +1,34 @@
+var uuid = require('node-uuid');
+const exec = require('child_process').exec;
+var fs = require('fs');
+
+module.exports = function getMlx906Reading(callback) {
+    var reading;
+    exec('sudo /home/pi/projects/mqtt_reader/pi_reader/scripts/eye2c', (error, stdout, stderr) => {
+        if (error) {
+            fs.appendFile('/home/pi/projects/mqtt_reader/pi_reader/errorLog.txt', 'mlx906 error-- data: ' + data, function (err) {
+            });
+            return callback(error);
+        }
+        if (stderr) {
+            fs.appendFile('/home/pi/projects/mqtt_reader/pi_reader/errorLog.txt', 'mlx906 error-- data: ' + data, function (err) {
+            });
+            return callback(error);
+        }
+        reading = `${stdout}`;
+        if(reading){
+            var obj = reading.split(',');
+            var objason = { 
+                createdAt : Date.now(), 
+                id : uuid.v4(), 
+                ip : "piSerial#", 
+                ok : true, 
+                sensor : "Mlx906", 
+                ambiTemp : obj[0].trim(),
+                skyTemp : obj[1].trim()
+            } 
+        }
+        //console.log(objason);
+        callback(null, objason, "Mlx906");
+    });
+}
