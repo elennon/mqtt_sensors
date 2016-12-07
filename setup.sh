ce5cfac820849e8eb74ac8a3e5ae0dd53c474d62
ed <elennon@outlook.ie>
2016-12-07 23:01:32 +0000

    set upper

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+
+# git clone https://github.com/elennon/mqtt_sensors.git
+# sudo apt-get install xrdp
+# sudo service xrdp start
+
+sudo apt-get update && sudo apt-get install git;
+
+#// node, nvm, npm
+wget https://nodejs.org/dist/v4.0.0/node-v4.0.0-linux-armv6l.tar.gz && tar -xvf node-v4.0.0-linux-armv6l.tar.gz && cd node-v4.0.0-linux-armv6l && sudo cp -R * /usr/local/;
+    
+       
+#nvm:
+sudo apt-get install build-essential libssl-dev && curl https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash && source ~/.profile;
+    
+#npm
+npm install npm@latest -g && nvm install v7.2.1;
+    
+
+// python
+sudo apt-get install -y python-smbus i2c-tools libi2c-dev python-rpi.gpio python3-rpi.gpio python-dev libglib2.0-dev python-pip build-essential
+
+
