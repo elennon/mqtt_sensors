63f06f05bd9f2e2f65e13aa7fc19a2ae656a96e2
ed <elennon@outlook.ie>
2016-12-07 23:28:02 +0000

    22

--- setup.sh
+++ setup.sh
@@ -8,14 +8,14 @@
 #sudo apt-get update && sudo apt-get install git;
 
 #// node, nvm, npm
-sudo apt-get update && sudo apt-get install git && wget https://nodejs.org/dist/v4.0.0/node-v4.0.0-linux-armv6l.tar.gz && tar -xvf node-v4.0.0-linux-armv6l.tar.gz && cd node-v4.0.0-linux-armv6l && sudo cp -R * /usr/local/;
+sudo apt-get update && sudo apt-get install git && echo 'done update' && wget https://nodejs.org/dist/v4.0.0/node-v4.0.0-linux-armv6l.tar.gz && tar -xvf node-v4.0.0-linux-armv6l.tar.gz && cd node-v4.0.0-linux-armv6l && sudo cp -R * /usr/local/ && echo 'done node';
     
        
 #nvm:
 sudo apt-get install build-essential libssl-dev && curl https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash && source ~/.profile;
     
 #npm
-npm install npm@latest -g && nvm install v7.2.1;
+npm install npm@latest -g && nvm install v7.2.1 && echo 'all done';
     
 
 // python
